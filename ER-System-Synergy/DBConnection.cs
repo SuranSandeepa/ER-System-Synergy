ce8ab31b00ce2ea1357965035efee6dd7c53defd
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2023-01-31 21:42:44 +0530

    DBConnection class added.

---
+++ ER-System-Synergy/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ER_System_Synergy
+{
+    public class DBConnection
+    {
+        public static string ConnectionString = "Data Source=DESKTOP-DCCUTP6\\SQLEXPRESS;Initial Catalog=MAS-KREEDA-Synergy-ER-App;Integrated Security=true";
+        //User ID = YourUsername; Password=YourPassword;
+    }
+}
