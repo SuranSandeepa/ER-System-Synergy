9896eb5c6e2a3b26a1502094109923f8d8f6b32a
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2023-02-01 12:03:46 +0530

    Excel Data Import to DB1 implemented

---
+++ ER-System-Synergy/ExcelUploadPage.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/NavBar.Master" AutoEventWireup="true" CodeBehind="ExcelUploadPage.aspx.cs" Inherits="ER_System_Synergy.ExcelUploadPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <br />
+
+    <div class="container alert alert-primary w3-padsding-top w3-padding-bottom w3-margin-top w3-margin-bottom" style="text-align: center; background-color: #28313B; color: white;">
+        <div class="row align-items-center">
+            <div class="col">
+                <asp:FileUpload ID="ExcelFileUploadDB1" runat="server" />
+            </div>
+            <div class="col">
+                <asp:Button class="btn" ID="UploadToDB1" Text="Upload to DB1" runat="server" OnClick="UploadExcelToDB1_Click" />
+            </div>
+            <div class="col">
+                <asp:Button class="btn" ID="ViewDB1" Text="View Database1" runat="server" OnClick="ViewDB1_Click"/>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ ER-System-Synergy/ExcelUploadPage.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace ER_System_Synergy
+{
+    public partial class ExcelUploadPage : System.Web.UI.Page
+    {
+
+        //Create a connection string to connect to the SQL Server database
+        string sqlConnectionString = DBConnection.ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadExcelToDB1_Click(object sender, EventArgs e)
+        {
+            if (ExcelFileUploadDB1.HasFile)
+            {
+                String d1date, d1pn, d1epf, d1name, d1gender, d1joindate, d1team, d1classification, d1shift;
+
+                string path = Path.GetFileName(ExcelFileUploadDB1.FileName);
+
+                path = path.Replace(" ", "");
+                ExcelFileUploadDB1.SaveAs(Server.MapPath("~/ExcelUpload/") + path);
+                String ExcelPath = Server.MapPath("~/ExcelUpload/") + path;
+                OleDbConnection mycon = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + ExcelPath + "; Extended Properties=Excel 8.0; Persist Security Info = False");
+                mycon.Open();
+                OleDbCommand cmd = new OleDbCommand("select * from [Sheet1$]", mycon);
+                OleDbDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    d1date = dr[0].ToString();
+                    d1pn = dr[1].ToString();
+                    d1epf = dr[2].ToString();
+                    d1name = dr[3].ToString();
+                    d1gender = dr[4].ToString();
+                    d1joindate = dr[5].ToString();
+                    d1team = dr[6].ToString();
+                    d1classification = dr[7].ToString();
+                    d1shift = dr[8].ToString();
+
+                    SaveExcelData(d1date, d1pn, d1epf, d1name, d1gender, d1joindate, d1team, d1classification, d1shift);
+
+                }
+
+                //Alert
+                Response.Write("<script>alert('Data Saved Successfully');</script>");
+
+                mycon.Close();
+
+                File.Delete(ExcelPath);
+
+            }
+            else
+            {
+                // Alert to show that no file has been selected
+                Response.Write("<script>alert('Please select a file');</script>");
+
+            }
+        }
+
+        private void SaveExcelData(String d1date, String d1pn, String d1epf, String d1name, String d1gender, String d1joindate, String d1team, String d1classification, String d1shift)
+        {
+            String query = "INSERT INTO ERDatabase1(d1date, d1pn, d1epf, d1name, d1gender, d1joindate, d1team, d1classification, d1shift ) values('" + d1date + "','" + d1pn + "','" + d1epf + "','" + d1name + "','" + d1gender + "','" + d1joindate + "','" + d1team + "','" + d1classification + "','" + d1shift + "')";
+            SqlConnection con = new SqlConnection(sqlConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+        }
+
+        protected void ViewDB1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Database1ViewPage.aspx");
+        }
+    }
+
+}
